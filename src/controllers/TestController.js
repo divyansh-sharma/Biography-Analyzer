006fba104dd1878a3e85fcb14bfd213ac94b4cf9
divyansh-sharma <divyansh2593@gmail.com>
2017-10-22 15:00:29 +0530

    changes to read uploaded files

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -5,7 +5,7 @@ import TestService from '../services/TestService';
 
 
 export default {
-  testMethod,helloWorld,getFiles,uploadFiles,getResults
+  testMethod,helloWorld,getFiles,uploadFiles,getResults,refresh
 };
 
 /**
@@ -13,25 +13,81 @@ export default {
  * @param {Object} req
  * @param {Object} res
  */
+function refresh(){
+	var fs= require('fs');
+	var path = require('path');
+	var fnames=[];
+	fnames=fs.readdirSync(path.resolve(__dirname, 'uploads/'));
+	for (var i=0; i<fnames.length; i++) {
+	fs.unlinkSync(path.resolve(__dirname, 'uploads/'+fnames[i]));
+    console.log('successfully deleted file '+path.resolve(__dirname, 'uploads/'+fnames[i]));
+
+}
+}
 async function helloWorld(req, res) {
   const result = await TestService.helloWorld();
   console.log("i m here");
   res.json(result);
   
 }
+/*function readFileContents(dirname){
+	var fs=require('fs');
+	var path=require('path');
+	return new Promise(function(resolve, reject) {
+		var fdata=[];
+		fs.readdir(path.resolve(__dirname, 'uploads/'), function(err, items) {
+    console.log(items);
 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+		
+		 fs.readFile(path.resolve(path.resolve(__dirname, 'uploads/'), items[i]), 'utf-8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  
+  fdata.push(data);
+  resolve(fdata);
+  //console.log(filedata); //logs values to the console
+  
+});
+   
+    
+		}
+	
+	
+}); 
+	
+	});
+} */
 async function getResults(req,res){
 var fs= require('fs');
 var path = require('path');
 console.log(JSON.stringify(req.body));
 var filedata = [];
+var fnames=[];
+/*readFileContents(path.resolve(__dirname, 'uploads/'))
+.then(function(fdata){
+	console.log(fdata); //print all file contents
+})
+.catch(function(err) {
+     console.log(err);
+   }); */
+fnames=fs.readdirSync(path.resolve(__dirname, 'uploads/'));
+for (var i=0; i<fnames.length; i++) {
+        console.log(fnames[i]);
+		console.log(path.resolve(__dirname, 'uploads/'+fnames[i]));
+		filedata.push(fs.readFileSync(path.resolve(__dirname, 'uploads/'+fnames[i]),'utf-8'));
+}
+console.log(filedata); // filedata is now a array of file contents
 
-fs.readdir(path.resolve(__dirname, 'uploads/'), function(err, items) {
+}
+/*fs.readdir(path.resolve(__dirname, 'uploads/'), function(err, items) {
     console.log(items);
 
     for (var i=0; i<items.length; i++) {
         console.log(items[i]);
-		fs.readFile(path.resolve(path.resolve(__dirname, 'uploads/'), items[i]), 'utf-8',function (err,data) {
+		 fs.readFile(path.resolve(path.resolve(__dirname, 'uploads/'), items[i]), 'utf-8',await function (err,data) {
   if (err) {
     return console.log(err);
   }
@@ -42,10 +98,7 @@ fs.readdir(path.resolve(__dirname, 'uploads/'), function(err, items) {
 });
     }
 	
-});
-console.log(filedata); //empty array,i want file data here for further processing
-
-}
+}); */
 async function uploadFiles(req,res){
 var path = require('path');
 var formidable = require('formidable');
@@ -78,6 +131,7 @@ var fs = require('fs');
 
 }
 async function getFiles(req,res) {
+  refresh();
   var path = require('path');
   res.sendFile(path.join(__dirname, 'views/index.html'));
 }
