d08eb2c09e02ef64df61340d225a4ec4f844e650
divyansh-sharma <divyansh2593@gmail.com>
2017-10-23 04:32:36 +0530

    added tone and personality

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -24,6 +24,66 @@ function refresh(){
 
 }
 }
+function analyzeTone(filedata){
+	
+	var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+    var tone_analyzer = new ToneAnalyzerV3({
+    username: 'cd4e8f70-e70a-4bb5-843c-105aa21b55e5',
+    password: 'qm8cDxSAX0a6',
+    version_date: '2016-05-19'
+});
+return new Promise(function(resolve, reject) {
+
+  var params = {
+  // Get the text from the JSON file.
+   text: filedata,
+   tones : 'emotion',
+   sentences: false
+   };  
+   tone_analyzer.tone(params, function(error, tones) {
+  if (error)
+    console.log('error:', error);
+  else
+  {//console.log(JSON.stringify(response, null, 2));
+   resolve(tones);
+  }
+  });
+
+
+});
+
+}
+function analyzePersonality(filedata){
+	
+	var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+   var personality_insights = new PersonalityInsightsV3({
+  username: '7c029f0f-8caa-4ace-8e51-223a19113c17',
+  password: 'nrwRQz14YOkN',
+  version_date: '2016-05-19'
+});
+return new Promise(function(resolve, reject) {
+
+ var params = {
+  // Get the content items from the JSON file.
+  text: filedata,
+  headers: {
+    'accept-language': 'en',
+    'accept': 'application/json'
+  }
+}; 
+   personality_insights.profile(params, function(error, profiles) {
+  if (error)
+    console.log('Error:', error);
+  else
+    //console.log(JSON.stringify(response, null, 2));
+    resolve(profiles);
+  }
+);
+
+
+});
+
+}
 async function helloWorld(req, res) {
   const result = await TestService.helloWorld();
   console.log("i m here");
@@ -60,12 +120,15 @@ async function helloWorld(req, res) {
 	
 	});
 } */
+
 async function getResults(req,res){
 var fs= require('fs');
 var path = require('path');
 console.log(JSON.stringify(req.body));
 var filedata = [];
+var translated_data=[];
 var fnames=[];
+var blahhh=[];
 /*readFileContents(path.resolve(__dirname, 'uploads/'))
 .then(function(fdata){
 	console.log(fdata); //print all file contents
@@ -80,7 +143,39 @@ for (var i=0; i<fnames.length; i++) {
 		filedata.push(fs.readFileSync(path.resolve(__dirname, 'uploads/'+fnames[i]),'utf-8'));
 }
 console.log(filedata); // filedata is now a array of file contents
+/*for (var i=0; i<filedata.length; i++) {
+translated_data.push(translate(filedata[i]));
+} */
+var tpromises=[];
+var ppromises=[];
+for(i=0;i<filedata.length;i++)
+{ 
+ tpromises.push(analyzeTone(filedata[i]));
+}
+Promise.all(tpromises).then(function(tones) {
+    console.log("done");
+	return res.json(tones);
+	//console.log(response);
+	//console.log(JSON.stringify(tones)); //use tones here
+	
+	
+});
 
+
+/*
+for(i=0;i<filedata.length;i++)
+{ 
+ ppromises.push(analyzePersonality(filedata[i]));
+}
+Promise.all(ppromises).then(function(profiles) {
+    console.log("done");
+	
+	//console.log(response);
+	//console.log(JSON.stringify(profiles)); //use tones here
+	
+}); 
+*/
+//personality=analyzePersonality(fildata);
 }
 /*fs.readdir(path.resolve(__dirname, 'uploads/'), function(err, items) {
     console.log(items);
